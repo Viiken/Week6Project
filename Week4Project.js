30acd1ab4f48a67cc05c9664302d6c1a025c9523
Laddie Hawkins <timothy.lad.hawkins@gmail.com>
2023-08-26 19:13:45 -0600

    added comments and fixed a few errors

--- Week4Project.js
+++ Week4Project.js
@@ -4,31 +4,47 @@ age = [3, 9, 23, 64, 2, 8, 28, 93]
 var sum = 0
 
 console.log(age[age.length-1] - age[0]);
+// last - first
 
 for(let i = 0; i < age.length; i++){
     sum += i;
+//going through and adding the ages
 }
 console.log(sum);
+console.log()
+//last - first after pushing a new age
+age.push(45)
+console.log(age[age.length-1] - age[0]);
+console.log()
 
 // 2.
 console.log('2.')
-names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
-var letters = 0;
+names = ['Sam','Tommy','Tim', 'Sally','Buck','Bob']
+var letters = 0
 
 for(let i = 0; i < names.length; i++){
-        for(let x = 0; x < names[i].length; x++){
-            letters = letters + x;
-        }
+    letters += names[i].length;
+// adding the lengths of the names together
 }
 console.log(letters/names.length)
+
+namesconcat = '';
+for(let i = 0; i < names.length; i++){
+  namesconcat += (names[i] + " ");
+}
+// adding the names together with as space after
+console.log(namesconcat)
+console.log()
 
 // 3. 
 console.log('3.')
 console.log('use the term nameOfArray.length -1')
+console.log()
 
 // 4. 
 console.log('4.')
 console.log('use nameOfArray[0]')
+console.log()
 
 // 5.
 console.log('5.') 
@@ -41,8 +57,10 @@ for(let i = 0; i < names.length; i++){
       number++;
     }
     nameLengths.push(number)
+//getting the number of letters in each name and adding them to a new array
 }
 console.log(nameLengths)
+console.log()
 
 // 6.
 console.log('6.')
@@ -51,9 +69,11 @@ function addArray (array){
     for(let i = 0; i < array.length; i++){
         answer += array[i]
     }
+// sum of the above array
     console.log(answer)
 }
 addArray(nameLengths)
+console.log()
 
 // 7.
 console.log('7.')
@@ -62,16 +82,20 @@ function concat (word, n){
     for(n; n != 0; n--){
         result += word
     }
+// empty string and then adding the word while n is not 0 counting down
     console.log(result)
 }
 concat('hi', 6)
+console.log()
 
 // 8.
 console.log('8.')
 function fullName (firstName, lastName){
     console.log(firstName + " " + lastName)
+// string + string with a space string
 }
-fullName('Frank', 'Sinatra')
+fullName('Anthony', 'Stark')
+console.log()
 
 //9. 
 console.log('9.')
@@ -86,10 +110,12 @@ function greaterThan100 (array){
     else{
       return false;
     }
+// finding lengths and then a boolean for if it is bigger than 100
 }
 array1 = [20,4,30,50,5]
 array2 = [21,5,30,50,5]
 console.log(greaterThan100(array1))
+console.log()
 
 // 10.
 console.log('10.')
@@ -99,8 +125,10 @@ function average (a){
         sum += a[i]
     }
     console.log(sum/a.length)
+// average of all the numbers in an array
 }
 average(array2)
+console.log()
 
 // 11.
 console.log('11.')
@@ -117,8 +145,10 @@ function greaterArray (a, b){
         return true;
     }
     else{return false}
+// finding lengths and then a boolean of which is longer
 }
 console.log(greaterArray(array1, array2))
+console.log()
 
 // 12.
 console.log('12.')
@@ -127,9 +157,11 @@ function willBuyDrink (isHotOutside, moneyInPocket){
         return true
     }
     else{return false}
+// satisfying two statements to return something
 }
 
 console.log(willBuyDrink(true, 15));
+console.log()
 
 // 13.
 console.log('13.');
